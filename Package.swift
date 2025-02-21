29e27bdda55915fd5a37128f69b8947e8b02a9d0
Surajkumbhar905 <skum@ciklum.com>
2025-02-21 15:40:34 +0530

    Add FrameWork

--- Package.swift
+++ Package.swift
@@ -15,23 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "WhetherEffectKit",
-            resources: [
-                        .process("RainFall.sks"),
-                        .process("SnowFall.sks"),
-                        .process("RainFallLanding.sks"),
-                        .process("SnowFallLanding.sks"),
-                        .process("spark.png"),
-                        .process("Ellipse1.png"),
-                        ]
-                        
-        ),
-        
-        .testTarget(
-            name: "WhetherEffectKitTests",
-            dependencies: ["WhetherEffectKit"]
-        ),
+        .binaryTarget(name: "WhetherEffectKit", path: "./Sources/WhetherEffectKit.xcframework")
         
     ]
 )
---
+++ Sources/WhetherEffectKit.xcframework/ios-arm64_x86_64-simulator/WhetherEffectKit.framework/Headers/WhetherEffectKit.h
@@ -0,0 +1,18 @@
+//
+//  WhetherEffectKit.h
+//  WhetherEffectKit
+//
+//  Created by Suraj-KU on 21/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WhetherEffectKit.
+FOUNDATION_EXPORT double WhetherEffectKitVersionNumber;
+
+//! Project version string for WhetherEffectKit.
+FOUNDATION_EXPORT const unsigned char WhetherEffectKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhetherEffectKit/PublicHeader.h>
+
+
